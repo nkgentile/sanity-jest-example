a362d52b09a42a9c83141270d457d653d9e74dc4
Noah Gentile <noah.gentile@gmail.com>
2022-09-06 14:01:58 -0700

    adding component test

---
+++ components/PreviewComponent.test.jsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import PreviewComponent from './PreviewComponent';
+
+test('renders the preview component', () => {
+  render(<PreviewComponent />);
+});
